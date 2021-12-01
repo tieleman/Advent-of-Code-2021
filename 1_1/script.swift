ee8a3151ca2073a1d5107453b00241f262069d9f
Sjoerd Tieleman <sjoerd@chordify.net>
2021-12-01 11:04:06 +0100

    Add comments

--- 1_1/script.swift
+++ 1_1/script.swift
@@ -4,11 +4,14 @@ import Algorithms // apple/swift-algorithms ~> 1.0.0
 
 // Load lines from file
 let data = try! String(contentsOfFile: "input.txt")
+
+// Massage the data
 let lines = data
     .components(separatedBy: .newlines)
     .filter({ !$0.isEmpty })
     .compactMap({ Int($0) })
 
+// Perform the count
 let count = lines.adjacentPairs().reduce(0) { partialResult, tuple in
     if tuple.1 > tuple.0 {
         return partialResult + 1
--- 1_2/script.swift
+++ 1_2/script.swift
@@ -4,13 +4,16 @@ import Algorithms // apple/swift-algorithms ~> 1.0.0
 
 // Load lines from file
 let data = try! String(contentsOfFile: "input.txt")
+
+// Massage the data
 let lines = data
     .components(separatedBy: .newlines)
     .filter({ !$0.isEmpty })
     .compactMap({ Int($0) })
     .windows(ofCount: 3)
-    .map({ $0.reduce(0, +)})
+    .map({ $0.reduce(0, +) })
 
+// Perform the count
 let count = lines.adjacentPairs().reduce(0) { partialResult, tuple in
     if tuple.1 > tuple.0 {
         return partialResult + 1
