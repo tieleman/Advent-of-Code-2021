f67bcea2b5456843971af4cda718fc3e78e08b23
Sjoerd Tieleman <sjoerd@chordify.net>
2021-12-02 09:22:07 +0100

    Use a more functional approach

--- 02a/script.swift
+++ 02a/script.swift
@@ -1,29 +1,48 @@
 #!/usr/bin/swift sh
 import Foundation
 
-// Load lines from file
-let data = try! String(contentsOfFile: "input.txt")
-
-// Massage the data
-let lines = data
-    .components(separatedBy: .newlines)
-    .filter({ !$0.isEmpty })
-
-// Perform the route
-var xPos = 0
-var yPos = 0
-
-lines.forEach { instruction in
-    let components = instruction.components(separatedBy: .whitespaces)
-    let submarineCommand = components[0]
-    let distance = Int(components[1])!
+enum SubmarineCommand {
+    case forward(Int)
+    case down(Int)
+    case up(Int)
     
-    switch submarineCommand {
-    case "forward": xPos += distance
-    case "down": yPos += distance
-    case "up": yPos -= distance
-    default: fatalError("Unknown command")
+    init?(_ rawCommand: String) {
+        let components = rawCommand.components(separatedBy: .whitespaces)
+        let submarineCommand = components[0]
+        
+        guard let distance = Int(components[1]) else { return nil }
+
+        switch submarineCommand {
+        case "forward": self = .forward(distance)
+        case "down": self = .down(distance)
+        case "up": self = .up(distance)
+        default: return nil
+        }
+    }
+    
+    func execute(_ current: SubmarinePosition) -> SubmarinePosition {
+        switch self {
+        case .forward(let distance):
+            return .init(depth: current.depth, xPos: current.xPos + distance)
+        case .down(let distance):
+            return .init(depth: current.depth + distance, xPos: current.xPos)
+        case .up(let distance):
+            return .init(depth: current.depth - distance, xPos: current.xPos)
+        }
     }
 }
 
-print(xPos * yPos)
+struct SubmarinePosition {
+    let depth: Int
+    let xPos: Int
+    
+    static let startingPoint = SubmarinePosition(depth: 0, xPos: 0)
+}
+
+let finalPosition = try! String(contentsOfFile: "input.txt")
+    .components(separatedBy: .newlines)
+    .filter({ !$0.isEmpty })
+    .compactMap({ SubmarineCommand($0) })
+    .reduce(SubmarinePosition.startingPoint) { partial, next in next.execute(partial) }
+
+print(finalPosition.depth * finalPosition.xPos)
