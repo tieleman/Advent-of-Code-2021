6b7c8d7e873f171177778ec1c6ed6c1d3aade5a1
Sjoerd Tieleman <sjoerd@chordify.net>
2021-12-07 08:30:01 +0100

    Add custom rounding to make both inputs pass

--- 07b/script.swift
+++ 07b/script.swift
@@ -5,7 +5,14 @@ let positions = try! String(contentsOfFile: "input.txt")
     .components(separatedBy: ",")
     .compactMap({ Int($0) })
 
-let targetPosition = positions.reduce(0, +) / positions.count
+let targetPosition : Int = {
+    let avg = Double(positions.reduce(0, +)) / Double(positions.count)
+    if avg - trunc(avg) > 0.5 {
+        return Int(ceil(avg))
+    } else {
+        return Int(floor(avg))
+    }
+}()
 
 let fuel = positions
     .map({ (1..<(abs($0 - targetPosition)+1)).reduce(0,+) })
