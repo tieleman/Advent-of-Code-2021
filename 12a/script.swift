76b01131225256da6d2df50cb3300c6ba91db0e4
Sjoerd Tieleman <sjoerd@chordify.net>
2021-12-12 10:21:53 +0100

    Remove passing of connections, since everything is already global anyway

--- 12a/script.swift
+++ 12a/script.swift
@@ -17,22 +17,22 @@ extension Cave {
     static var start: Cave = "start"
     static var end: Cave   = "end"
     
-    func connectedCaves(_ connections: [Connection]) -> [Cave] {
+    var connectedCaves: [Cave] {
         connections.filter({ $0.from == self || $0.to == self })
             .flatMap({ [$0.to, $0.from] })
             .filter({ $0 != self })
     }
 }
 
-func generateAllRoutes(from connections: [Connection]) {
+func generateAllRoutes() {
     var visited = Route()
     
     visited.append(Cave.start)
 
-    generateRoute(from: connections, visited: visited)
+    generateRoute(visited: visited)
 }
 
-func generateRoute(from connections: [Connection], visited: Route) {
+func generateRoute(visited: Route) {
     var localVisited = visited
     
     if localVisited.last! == Cave.end {
@@ -40,10 +40,10 @@ func generateRoute(from connections: [Connection], visited: Route) {
         return
     }
     
-    visited.last!.connectedCaves(connections).forEach { cave in
+    visited.last!.connectedCaves.forEach { cave in
         if (!visited.contains(cave) || cave.canBeRevisitted) {
             localVisited.append(cave)
-            generateRoute(from: connections, visited: localVisited)
+            generateRoute(visited: localVisited)
             localVisited.popLast()
         }
     }
@@ -58,6 +58,6 @@ let connections = try! String(contentsOfFile: "input.txt")
 
 var routes = [Route]()
 
-generateAllRoutes(from: connections)
+generateAllRoutes()
 
 print(routes.count)
--- 12b/script.swift
+++ 12b/script.swift
@@ -27,22 +27,22 @@ extension Cave {
     static var start: Cave = "start"
     static var end: Cave   = "end"
     
-    func connectedCaves(_ connections: [Connection]) -> [Cave] {
+    var connectedCaves: [Cave] {
         connections.filter({ $0.from == self || $0.to == self })
             .flatMap({ [$0.to, $0.from] })
             .filter({ $0 != self })
     }
 }
 
-func generateAllRoutes(from connections: [Connection]) {
+func generateAllRoutes() {
     var visited = Route()
     
     visited.append(Cave.start)
 
-    generateRoute(from: connections, visited: visited)
+    generateRoute(visited: visited)
 }
 
-func generateRoute(from connections: [Connection], visited: Route) {
+func generateRoute(visited: Route) {
     var localVisited = visited
     
     if localVisited.last! == Cave.end {
@@ -50,11 +50,11 @@ func generateRoute(from connections: [Connection], visited: Route) {
         return
     }
     
-    visited.last!.connectedCaves(connections).forEach { cave in
+    visited.last!.connectedCaves.forEach { cave in
         if (cave.canBeRevisitted || !visited.contains(cave)) {
             if cave.isSmallCave { visitedCount[cave, default: 0] += 1 }
             localVisited.append(cave)
-            generateRoute(from: connections, visited: localVisited)
+            generateRoute(visited: localVisited)
             if cave.isSmallCave { visitedCount[cave, default: 0] -= 1 }
             localVisited.popLast()
         }
@@ -71,6 +71,6 @@ let connections = try! String(contentsOfFile: "/Users/tieleman/Documents/Code/Ao
 var routes = [Route]()
 var visitedCount = [Cave: Int]()
 
-generateAllRoutes(from: connections)
+generateAllRoutes()
 
 print(routes.count)
